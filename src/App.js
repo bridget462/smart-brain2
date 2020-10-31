bfde93f562095534b3e0e2b940120c0d070f8c67
bridget462 <bridges.dev.create@gmail.com>
2020-10-31 16:54:42 +0900

    Rank component created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import "./App.css";
 import Navigation from "./components/Navigation/Navigation.js";
 import Logo from "./components/Logo/Logo.js";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+
+const particleOptionParams = {
+  particles: {
+    number: {
+      value: 50,
+    },
+    size: {
+      value: 3,
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particleOptionParams} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -9,9 +9,9 @@ const Logo = () => {
       <Tilt
         className="Tilt br2 shadow-2"
         options={{ max: 55 }}
-        style={{ height: 250, width: 250 }}
+        style={{ height: 150, width: 150 }}
       >
-        <div className="Tilt-inner pa5">
+        <div className="Tilt-inner pa4">
           <img alt="logo" src={brain} />
         </div>
       </Tilt>
